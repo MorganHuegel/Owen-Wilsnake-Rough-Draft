e8756333a87091d122c5db975b5db8b3091eabd9
MorganHuegel <mhue2910@gmail.com>
2019-07-19 21:08:46 -0400

    refactoring Gameplay components, created GameplayMain2

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { GameplayMain } from './gameplay/GameplayMain';
+import { GameplayMain } from './gameplay2/GameplayMain';
 import { LandingMain } from './landing-page/LandingMain';
 
 
---
+++ src/components/gameplay2/GameplayMain.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { Header } from './Header';
+
+/// LOAD THE SOUND BYTES //////////////////////////////////////
+let Sound = require('react-native-sound');
+Sound.setCategory('Playback'); // Enable playback in silence mode
+const soundsFolder = '../../../sounds'
+let soundBytes = [
+  require(`${soundsFolder}/owen-wilson-saying-wow.mp3`),
+  require(`${soundsFolder}/owen-wilson-saying-wow-1.m4a`),
+  require(`${soundsFolder}/owen-wilson-saying-wow-2.m4a`),
+  require(`${soundsFolder}/owen-wilson-saying-wow-3.m4a`),
+  require(`${soundsFolder}/owen-wilson-saying-wow-4.m4a`),
+  require(`${soundsFolder}/owen-wilson-saying-wow-5.m4a`),
+  require(`${soundsFolder}/owen-wilson-saying-wow-6.m4a`),
+  require(`${soundsFolder}/owen-wilson-saying-wow-7.m4a`),
+  require(`${soundsFolder}/owen-wilson-saying-wow-8.m4a`),
+  require(`${soundsFolder}/owen-wilson-saying-wow-9.m4a`),
+  require(`${soundsFolder}/owen-wilson-saying-wow-10.m4a`),
+  require(`${soundsFolder}/owen-wilson-saying-wow-11.m4a`),
+  require(`${soundsFolder}/owen-wilson-saying-wow-12.m4a`),
+  require(`${soundsFolder}/owen-wilson-saying-wow-13.m4a`)
+]
+////////////////////////////////////////////////////////////////
+
+export class GameplayMain extends React.Component {
+  state = {
+    owenSoundIndex: 0,
+  }
+
+  soundByte = new Sound(soundBytes[this.state.owenSoundIndex])
+
+  playOwenSound = () => {
+    this.soundByte.play()
+
+    const nextSoundByte = 
+      (this.state.owenSoundIndex === soundBytes.length - 1)
+      ? 0 : this.state.owenSoundIndex + 1
+
+    this.setState({owenSoundIndex: nextSoundByte}, () => {
+      this.soundByte = new Sound(soundBytes[this.state.owenSoundIndex], err => {
+        if (err) {
+          console.log(err)
+        }
+      })
+    })
+  }
+
+
+  render(){
+    return (
+      <View>
+        <Header/>
+        <Text>Gameplay2</Text>
+      </View>
+    )
+  }
+}
