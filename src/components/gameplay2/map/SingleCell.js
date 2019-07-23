efffc7c6e3a66a3d60dd6b50f02eabc304772050
MorganHuegel <mhue2910@gmail.com>
2019-07-23 14:02:10 -0400

    cleaned code

--- src/components/gameplay2/map/SingleCell.js
+++ src/components/gameplay2/map/SingleCell.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 export function SingleCell (props) {
-  console.log(props)
   return (
     <Text style={props.singleStylesheet}>1</Text>
   )
