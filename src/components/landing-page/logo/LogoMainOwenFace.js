32dc49f067d1a2cb6e723d4b949517ac73946f6f
MorganHuegel <mhue2910@gmail.com>
2019-08-11 15:14:32 -0400

    udpated face animations in logo so that parent component controls animations

--- src/components/landing-page/logo/LogoMainOwenFace.js
+++ src/components/landing-page/logo/LogoMainOwenFace.js
@@ -2,42 +2,11 @@ import React from 'react';
 
 import { Animated } from 'react-native';
 
-export class LogoMainOwenFace extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      bottom: new Animated.Value(props.containerHeight * 1/6)
-    }
-    this.styling = {
-      ...this.props.styleProps,
-      bottom: this.state.bottom
-    }
-  }
-
-  componentDidMount(){
-    setTimeout( () => {
-      Animated.loop(
-        Animated.sequence([
-          Animated.timing(this.state.bottom, {
-            toValue: this.props.containerHeight * 1/6 + 40,
-            duration: 1000
-          }),
-          Animated.timing(this.state.bottom, {
-            toValue: this.props.containerHeight * 1/6,
-            duration: 1000
-          })
-        ])
-      ).start()      
-    }, this.props.animationDelay)
-  }
-
-  render(){
-    console.log('IN HERE', this.styling)
-    return (
-      <Animated.Image 
-        source={require('./owen-face-with-border.png')} 
-        style={this.styling}
-      />
-    )
-  }
+export function LogoMainOwenFace (props) {
+  return (
+    <Animated.Image 
+      source={require('./owen-face-with-border.png')} 
+      style={props.styleProps}
+    />
+  )
 }
